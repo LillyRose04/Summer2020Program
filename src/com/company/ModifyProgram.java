a48a7cc8c92576c93678f1ee401a5e382401d9dd
lillythieberg <lthieberg@gmail.com>
2020-06-21 18:22:28 -0500

    This is my resubmission of the program for a grade change

--- src/com/company/ModifyProgram.java
+++ src/com/company/ModifyProgram.java
@@ -1,30 +1,26 @@
 package com.company;
 /* Lilly Thieberg 6/10/20 Modify program with constants and integers- version 2 contains some simplfied values*/
 public class ModifyProgram {
-    public static final int Amount1= 38;
-    public static final int Amount2= 40;
-    public static final int Amount3= 30;
-    public static final double Tax= .08;
-    public static final double Tip= .15;
 
 
         public static void main(String[] args) {
+            int Amount1= 38; // so that the amounts can be change if needed
+            int Amount2= 40;
+            int Amount3= 30;
+            double Tax= .08;
+            double Tip= .15;
 
+            int Subtotal= Amount1+Amount2+Amount3; // calculate the subtoal of the bill - it is an int
             // Calculate total owed, assuming 8% tax / 15% tip
-            double Subtotal= Amount1+Amount2+Amount3;
+            double Total = Subtotal + Tax + Tip;
+
             System.out.println("Subtotal: "+ Subtotal);
 
-            System.out.println("Tax:");
+            System.out.println("Tax: " + Tax);
 
-            System.out.println((Subtotal) * Tax);
+            System.out.println("Tip: " + Tip);
 
-            System.out.println("Tip:");
-
-            System.out.println((Subtotal) * Tip);
-
-            System.out.println("Total:");
-
-            System.out.println(Subtotal + (Subtotal) * Tip + (Subtotal) * Tax);
+            System.out.println("Total: " + Total);
 
         }
 
