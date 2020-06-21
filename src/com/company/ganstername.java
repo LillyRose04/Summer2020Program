018be1ec39f5ef890ee31a9022ee3613b33d7c77
lillythieberg <lthieberg@gmail.com>
2020-06-21 18:34:40 -0500

    This is my resubmission of the program for a grade change

--- src/com/company/ganstername.java
+++ src/com/company/ganstername.java
@@ -1,18 +1,18 @@
 package com.company;
 
-import java.util.Scanner;
+import java.util.Scanner; // import the scanner, could also use import java.util.*; if i wanted
 
 /* Lilly Thieberg 6/10/20 Ganster name code*/
 public class ganstername {
     public static void main(String args[]) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Please input your name");
-        String name = input.nextLine();
-        String first = name.substring (0,name.indexOf(" "));
-                String last = name.substring(name.indexOf(" ")+1);
-                last = last.toUpperCase();
-                String fInitial = first.substring(0,1);
-                System.out.println("Your ganster name is \"" + fInitial + ". Diddy " + last + " " + first + "-izzle\"");
+        Scanner input = new Scanner(System.in); // intializing scanner
+        System.out.print("Please input your name"); // asking the user to input their name
+        String name = input.nextLine(); // defrine the input as name
+        String first = name.substring (0,name.indexOf(" ")); //uses indexof to carve out the first name because it starts as 0 and end at a space
+                String last = name.substring(name.indexOf(" ")+1); // this mane there last mane a string
+                last = last.toUpperCase(); // makes their last name uppercase
+                String fInitial = first.substring(0,1); // make ther fisrt intial a string by using substring
+                System.out.println("Your ganster name is \"" + fInitial + ". Diddy " + last + " " + first + "-izzle\""); // puts all of the string together with the other words in the ganster name
 
     }
 }
