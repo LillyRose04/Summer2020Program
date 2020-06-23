4a524faafbfe63777c61e25cb5772d3bd5ece9be
lillythieberg <lthieberg@gmail.com>
2020-06-22 22:17:08 -0500

    this is an update

--- src/com/company/Stringchallengearray.java
+++ src/com/company/Stringchallengearray.java
@@ -6,19 +6,17 @@ import java.util.Scanner;
 Lilly Thieberg 6/21/2020
 import scanner
 initalize scanner
-declare string object word
-declare t as my varible for number of words
-open a do statment
-ask them in input a number between 1 and 10
-give T qith next screen as the varible that was inputted
-clos do and say while for t between 1-10 so that is they input a number a value not between the constants they have to input a new one
-open for loop for inoutting the word and set contraints for the number of time the loop runs
-open do loop
-ask them to input the word
-"collect word as the varible word useinf screen.next
-close do stament then use while and contran th e world lenth to bewtten 2 and 10000
-creat a string split = dividing(word); dividing is the new method
-print Split
+declare t as my varible int eqaul to 10
+make an array = to srting[t]
+ask them to input a word more then 5 charters long
+make an index ( in a for loop)nor a varible i that will index the array
+appile i to the array and collect the inputed values
+close the loop
+print a gap for nice spacing :)
+create a new array  equal to new String[word.length] this new array  will end up containg the fished word
+make an index i (in a for loop) to apply to the new array
+make the new array equal to your method and the array word[i]
+pint out the new array Words[i]
 method dividing
 declare string evens and odds ti equal ""
 open for loop contran the int even to equal 0 be less then word then and the intial it to it get the even number
@@ -33,23 +31,19 @@ then return awnser
 public class Stringchallengearray {
     public static void main(String[] args) {
         Scanner screen = new Scanner(System.in);
-        String word;
-        int T;
-        do {
-            System.out.println("How many words will you input (must be between 1 and 10)");
-            T = screen.nextInt(); // takle what was inputed as T
-        } while (T < 1 || T > 10); // must be between the 2 number in not then it will ask the user again
-
-        for (int i = 1; i <= T; i++) { // number of times it will ask you to input words
-            do {
-                System.out.println("Input your word:");
-                word = screen.next(); // takes what was inputed as word
-            } while (word.length() < 2 || word.length() >= 10000); // makes sure the lenth of the word is between 2 and 10000
-            String Split = dividing(word); // new strin that calls to the methos that will spelit the word and return the anwser
-            System.out.println(Split); // the printing of the string answer from the method
+        int T = 10;
+        String[] word = new String[T]; // this is the declaration =of an array that will collect the words inputed evutally
+        System.out.println("input a word (at least 5 charters please)");
+        for (int i = 0; i < word.length; i++) { // this given the index of the array and intilizes it wil the ++
+            word[i] = screen.next(); // collect the words as word with the i index
+        }
+        System.out.println(); // give a gap between the inputed words and the out[uted word it looks better this was
+        String[] Words = new String[word.length]; // take the inputed values from the array in a new stirng
+        for (int i = 0; i < word.length; i++ ){ // gives an index for i
+            Words[i]= dividing(word[i]); // applies the index i to the stind words and use the method divding with will slip up ther words
+            System.out.println(Words[i]); //. print the final words
         }
     }
-
     public static String dividing(String word) {
         String evens = "";
         String odds = "";
@@ -64,7 +58,6 @@ public class Stringchallengearray {
 
         String answer = evens + " " + odds; // the string gives the even and the odds with a space in bewteen
         return answer; // return the string to be printed
+
     }
-
-
 }
